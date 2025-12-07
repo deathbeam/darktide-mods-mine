28198344c27fad2fc6ad40e95b75640d0c7d19b8
Tomas Slusny <slusnucky@gmail.com>
2025-12-07 16:22:47 +0100

    Add ScanHelper
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

---
+++ mods/ScanHelper/scripts/mods/ScanHelper/ScanHelper.lua
@@ -0,0 +1,28 @@
+﻿local mod = get_mod("ScanHelper")
+
+local scannable_units = {}
+
+local _set_outline_and_highlight = function(active)
+    for scannable_unit, _ in pairs(scannable_units) do
+        local scannable_extension = ScriptUnit.has_extension(scannable_unit, "mission_objective_zone_scannable_system")
+
+        if scannable_extension then
+            local is_active = scannable_extension:is_active()
+
+            if is_active then
+                scannable_extension:set_scanning_outline(active)
+                scannable_extension:set_scanning_highlight(active)
+            end
+        end
+    end
+end
+
+mod:hook_safe(CLASS.AuspexScanningEffects, "init", function (...)
+    local mission_objective_zone_system = Managers.state.extension:system("mission_objective_zone_system")
+    
+    scannable_units = mission_objective_zone_system:scannable_units()
+
+    if scannable_units then
+        _set_outline_and_highlight(true)
+    end
+end)
---
+++ mods/ScanHelper/scripts/mods/ScanHelper/ScanHelper_localization.lua
@@ -0,0 +1,10 @@
+return {
+	mod_description = {
+		en = "Add a conspicuous mark to the object to be scanned",
+		ru = "Добавьте заметную метку на сканируемый объект",
+		["zh-tw"] = "在待掃描物件上加入醒目的標記",
+		["zh-cn"] = "给需要扫描的物体加个显眼的标记",
+		["pt-pt"] = "Adicione uma marca visível ao objeto a ser digitalizado",
+		["pt-br"] = "Adicione uma marca visível ao objeto a ser digitalizado",
+	},
+}
