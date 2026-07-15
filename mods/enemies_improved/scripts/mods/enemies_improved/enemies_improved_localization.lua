d436cbc866a766d680fc6bd14ba6218f65e651f0
Tomas Slusny <slusnucky@gmail.com>
2026-07-15 15:31:18 +0200

    update mods
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- mods/enemies_improved/scripts/mods/enemies_improved/enemies_improved_localization.lua
+++ mods/enemies_improved/scripts/mods/enemies_improved/enemies_improved_localization.lua
@@ -1,5 +1,5 @@
 local mod = get_mod("enemies_improved")
-mod.version = "2.0.6"
+mod.version = "2.0.7"
 mod:info("Enemies Improved is installed, using version: " .. tostring(mod.version))
 
 local next = next
