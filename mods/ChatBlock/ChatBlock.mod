7211ed014411475d25d51d2b35cd804afa1e3cf5
Tomas Slusny <slusnucky@gmail.com>
2026-08-05 09:24:54 +0200

    add chatblock
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

---
+++ mods/ChatBlock/ChatBlock.mod
@@ -0,0 +1,14 @@
+return {
+	run = function()
+		fassert(rawget(_G, "new_mod"), "`ChatBlock` encountered an error loading the Darktide Mod Framework.")
+
+		new_mod("ChatBlock", {
+			mod_script       = "ChatBlock/scripts/mods/ChatBlock/ChatBlock",
+			mod_data         = "ChatBlock/scripts/mods/ChatBlock/ChatBlock_data",
+			mod_localization = "ChatBlock/scripts/mods/ChatBlock/ChatBlock_localization",
+		})
+	end,
+	packages = {},
+	version = "26.08.05",
+	mod_id = "68",
+}
---
+++ mods/ChatBlock/scripts/mods/ChatBlock/ChatBlock_localization.lua
@@ -0,0 +1,15 @@
+return {
+	mod_name = {
+		en = "Chat Block",
+		ru = "Чат Блок",
+		["zh-cn"] = "聊天自动格挡",
+		["zh-tw"] = "聊天自動格擋",
+	},
+	mod_description = {
+		en = "Automatically hold block while chatting, opening the Steam overlay, or while the game is not focussed",
+		["zh-cn"] = "在打开聊天、打开 Steam 游戏内界面或者切换到其他窗口时自动格挡",
+ 		ru =
+ 		"Chat Block - Автоматически удерживается блок во время использования чата, при открытии оверлея Steam или когда игра не находится в фокусе.",
+		["zh-tw"] = "聊天、開啟 Steam 內嵌介面，或焦點不在遊戲視窗時自動維持格擋",
+	},
+}
